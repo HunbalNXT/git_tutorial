63db824e6d2648d2fad2f001ba29fdc5ac3b3e73
Hunbal Siddiqui <hunbal@nxtdevs.com>
2022-10-18 11:32:11 +0500

    auth work done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          In auth branch abc work...
         </a>
       </header>
     </div>
